8c3bc9749f7f6c52ba8542f5ad626d0142ad1f83
jordido <jordi@zona-e.com>
2014-10-23 19:45:06 +0200

    Removed email column and created again as index

---
+++ db/migrate/20141023173124_add_field_email_to_table_user.rb
@@ -0,0 +1,6 @@
+class AddFieldEmailToTableUser < ActiveRecord::Migration
+  def change
+    add_column :users, :email, :string
+    add_index :users, :email
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,9 +11,26 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20141023173124) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "users", force: true do |t|
+    t.string   "name"
+    t.string   "last_name"
+    t.string   "address"
+    t.string   "city"
+    t.string   "region"
+    t.string   "country"
+    t.string   "description"
+    t.string   "website_url"
+    t.string   "password_digest"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.string   "email"
+  end
+
+  add_index "users", ["email"], name: "index_users_on_email", using: :btree
 
 end
