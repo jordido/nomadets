4f18ac830f02bac175008ef7e7a9ed031dd9734e
jordido <jordi@zona-e.com>
2014-10-25 15:52:53 +0200

    created categories table

--- app/models/course.rb
+++ app/models/course.rb
@@ -1,2 +1,7 @@
-class Course < ActiveRecord::Base
+class Course < ActiveRecord::Base	
+	belongs_to category
+	belongs_to venue
+	belongs_to teacher
+	has_many students
+	has_many reviews
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,12 +11,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141023210843) do
+ActiveRecord::Schema.define(version: 20141024153835) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "admin_users", force: true do |t|
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "categories", force: true do |t|
+    t.string   "name"
+    t.string   "icon"
+    t.integer  "parent_id"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
