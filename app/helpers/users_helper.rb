aba97ecac440327a05b42dd270f97c49a5c06c4a
jordido <jordi@zona-e.com>
2014-10-29 00:58:34 +0100

    changed relations in reviews and users in order to improve usability :reviewed_by and :reviewed_to

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,9 +1,3 @@
 module UsersHelper
-	def reviewed_by(user)
-		 Review.find_by_author_id(user).comment if Review.find_by_author_id(user)
-	end
-
-	def reviewed(user)
-	end
 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,10 +2,11 @@ class User < ActiveRecord::Base
 	TYPES = ["Teacher","Venue","Student"]
 	has_secure_password
 
+	has_many :reviews
+	has_many :reviews_by, class_name: "Review", foreign_key: "author_id"
+	has_many :reviews_to, class_name: "Review", foreign_key: "reviewed_id"
+	has_many :courses, class_name: "Course", foreign_key: "courses_id"
+
 	validates :email, presence: true, uniqueness: true, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i } 
 	validates_presence_of :password_digest
-
-	has_many :reviews
-	has_many :courses
-
 end
