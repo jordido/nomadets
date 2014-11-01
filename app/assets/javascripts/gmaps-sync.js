8d27fb516a417bf821b1a6c9a77b86881cbe7d68
jordido <jordi@zona-e.com>
2014-11-01 20:32:47 +0100

    added tables of cities, region and countries, imported from google, changed fields in users to index them

---
+++ app/assets/javascripts/gmaps-sync.js
@@ -0,0 +1,95 @@
+var default_map_center_latitude = 41.3810212;
+var default_map_center_longitude = 2.1681336;
+var default_general_zoom = 14
+var default_place_zoom = 20
+var map;
+var ROOT_URL = 'http://localhost:3000';
+var infowindow;
+var places = [];
+
+function initialize(places) {
+ 	var mapOptions = {
+    center: new google.maps.LatLng(default_map_center_latitude, default_map_center_longitude),
+    zoom: default_general_zoom,
+    mapTypeId: google.maps.MapTypeId.ROADMAP,
+    panControl: true,
+    scaleControl: true,
+    streetViewControl: true,
+    overviewMapControl: true,
+    scrollwheel: true
+  };
+  infowindow = new google.maps.InfoWindow();
+	map = new google.maps.Map(document.getElementById('map-canvas'),
+      mapOptions);
+	var i;
+	for (i in places) { 
+		var marker = new google.maps.Marker( {
+	    position: places[i].coords,
+	    draggable: true,
+	    map: map,
+	    // icon: 'map-pin-green.png',
+	    title: "prueba de pin"
+		});
+		makeInfoWindowEvent(map, infowindow, places[i].name, places[i].url, marker);
+	}
+	setBounds(places);
+}
+
+function loadScript() {
+  var script = document.createElement('script');
+  script.type = 'text/javascript';
+  script.src = 'https://maps.googleapis.com/maps/api/js?v=3.exp&' +
+      'callback=get_coords';
+  document.body.appendChild(script);
+}
+
+//$(document).ready(loadScript);
+$(window).load(loadScript);
+
+function get_coords() {
+  $.ajax({
+      type: "GET",
+      url: 'map'
+
+    }).done(function(data,textStatus, jqXHR) {
+    		var i;
+//        var places = [];
+        for (i in data) { 
+          var place = {};
+          place["id"] = data[i].id
+          place["url"] = data[i].website_url;
+          place["name"] = data[i].name + " " + data[i].last_name 
+          // place["lat"] = data[x].latitude;
+          // place["lng"] = data[x].longitude;
+//          var coords = new google.maps.LatLng(data[x].latitude, data[x].longitude);
+          place["coords"] = new google.maps.LatLng(data[i].latitude, data[i].longitude);
+          places.push(place);
+        }
+        console.log(places);
+        initialize(places);
+
+    }).fail(function(jqXHR, textStatus, errorThrown) {
+        alert( textStatus );
+      
+    }).always(function() { 
+        
+    });
+}
+
+function setBounds(markersArray) {
+
+    var bounds = new google.maps.LatLngBounds();
+    for (var i=0; i < markersArray.length; i++) {
+        bounds.extend(markersArray[i].coords);
+    }
+    map.fitBounds(bounds);
+}
+
+function makeInfoWindowEvent(map, infowindow, contentString, url, marker) {
+  google.maps.event.addListener(marker, 'click', function() {
+  	content = '<p id="hook">' + contentString + '</p>';
+  	whole_content = '<a href='+ url + '>' + content + '</a>';
+    infowindow.setContent(whole_content);
+    infowindow.open(map, marker);
+  });
+}
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141030215354) do
+ActiveRecord::Schema.define(version: 20141101192548) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -39,6 +39,39 @@ ActiveRecord::Schema.define(version: 20141030215354) do
   add_index "categories_users", ["category_id"], name: "index_categories_users_on_category_id", using: :btree
   add_index "categories_users", ["user_id"], name: "index_categories_users_on_user_id", using: :btree
 
+  create_table "cities", primary_key: "CityId", force: true do |t|
+    t.integer "country_id",            null: false
+    t.integer "region_id",             null: false
+    t.string  "name",       limit: 45, null: false
+    t.float   "latitude",              null: false
+    t.float   "longitude",             null: false
+    t.string  "timezone",   limit: 10, null: false
+    t.integer "dma_id"
+    t.string  "code",       limit: 4
+  end
+
+  add_index "cities", ["name"], name: "index_cities_on_name", using: :btree
+
+  create_table "countries", primary_key: "CountryId", force: true do |t|
+    t.string  "name",                 limit: 50, null: false
+    t.string  "fips104",              limit: 2,  null: false
+    t.string  "iso2",                 limit: 2,  null: false
+    t.string  "iso3",                 limit: 3,  null: false
+    t.string  "ison",                 limit: 4,  null: false
+    t.string  "internet",             limit: 2,  null: false
+    t.string  "capital",              limit: 25
+    t.string  "map_reference",        limit: 50
+    t.string  "nationality_singular", limit: 35
+    t.string  "nationality_plural",   limit: 35
+    t.string  "currency",             limit: 30
+    t.string  "currency_code",        limit: 3
+    t.integer "population"
+    t.string  "title",                limit: 50
+    t.string  "comment"
+  end
+
+  add_index "countries", ["name"], name: "index_countries_on_name", using: :btree
+
   create_table "courses", force: true do |t|
     t.string   "name"
     t.string   "subject"
@@ -51,6 +84,15 @@ ActiveRecord::Schema.define(version: 20141030215354) do
     t.integer  "venue_id"
     t.integer  "teacher_id"
   end
+
+  create_table "regions", primary_key: "RegionId", force: true do |t|
+    t.integer "country_id",            null: false
+    t.string  "name",       limit: 45, null: false
+    t.string  "code",       limit: 8,  null: false
+    t.string  "adm1code",   limit: 4,  null: false
+  end
+
+  add_index "regions", ["name"], name: "index_regions_on_name", using: :btree
 
   create_table "reviews", force: true do |t|
     t.string   "comment"
@@ -70,9 +112,7 @@ ActiveRecord::Schema.define(version: 20141030215354) do
     t.string   "name"
     t.string   "last_name"
     t.string   "address"
-    t.string   "city"
-    t.string   "state"
-    t.string   "country"
+    t.string   "town"
     t.string   "description"
     t.string   "website_url"
     t.string   "password_digest"
@@ -83,8 +123,15 @@ ActiveRecord::Schema.define(version: 20141030215354) do
     t.string   "picture"
     t.float    "latitude"
     t.float    "longitude"
+    t.boolean  "gmaps"
+    t.integer  "country_id"
+    t.integer  "region_id"
+    t.integer  "city_id"
   end
 
+  add_index "users", ["city_id"], name: "index_users_on_city_id", using: :btree
+  add_index "users", ["country_id"], name: "index_users_on_country_id", using: :btree
   add_index "users", ["email"], name: "index_users_on_email", using: :btree
+  add_index "users", ["region_id"], name: "index_users_on_region_id", using: :btree
 
 end
