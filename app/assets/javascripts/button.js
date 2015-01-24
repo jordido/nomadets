1285a566837f6cc9caaeaec83ccaeec126f24f98
Karolinka <li-nee@gmx.de>
2015-01-24 12:04:17 +0100

    searchform

---
+++ app/assets/javascripts/button.js
@@ -0,0 +1,23 @@
+// var ready;
+
+
+// ready = function() {
+
+//     console.log("heey");
+
+
+
+//     $(".btn-toogle").click(function() {
+//         $("this").toggleClass("collapsed");
+
+//     })
+
+
+// }
+
+// $(document).ready(ready);
+// $(document).on('page:load', ready);
+
+// // if ($(this).hasClass("collapsed")) {
+// //     alert("cool");
+// // }
